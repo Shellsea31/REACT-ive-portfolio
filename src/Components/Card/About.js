9310c25247f0622fbdac743b429d3941913fa36e
Shelsy Elaine <elainesbarrera@gmail.com>
2021-03-03 10:16:44 -0800

    FIX: homepage link

--- src/Components/Card/About.js
+++ src/Components/Card/About.js
@@ -2,19 +2,19 @@ import React from "react";
 
 const About = () => {
   const style = { margin: "25px" };
-  const imgStyle = { height: "300px", width: "auto" };
+  const imgStyle = { height: "300px", width: "300px", margin: "20px" };
   return (
     <div className="container">
       <div style={style} className="row">
-        <img
-          style={imgStyle}
-          className="float-left"
-          id="image"
-          src="https://avatars.githubusercontent.com/u/70654835?s=460&u=aea06b3c752ee1ecf1b49ee0f91df119208fa516&v=4"
-          alt="PictureShelsy"
-        />
+        <div className="col-sm-12">
+          <img
+            style={imgStyle}
+            className="float-start"
+            id="image"
+            src="https://avatars.githubusercontent.com/u/70654835?s=460&u=aea06b3c752ee1ecf1b49ee0f91df119208fa516&v=4"
+            alt="PictureShelsy"
+          />
 
-        <div className="row">
           <p>
             Hi! My name is Shelsy Barrera and I'm a 21 year old student from San
             Jose, CA. On this site you'll find examples of some of my best work
@@ -23,9 +23,7 @@ const About = () => {
             discipline and determination. I've accomplished tasks in time I
             never thought possible.
           </p>
-        </div>
 
-        <div className="row">
           <p>
             Straight out of highschool, I studied Medical Assisting for two
             years and quickly realized it wasn't my career of choice. I
